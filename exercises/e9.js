39c9a372c1c50d44e2103af066ccffdb1481f9f8
Ceech Hsu <ceechhsu@gmail.com>
2023-02-23 14:22:46 -0800

    passed test-9

--- exercises/e9.js
+++ exercises/e9.js
@@ -1,4 +1,3 @@
-
 // EXERCISE 9
 // Return an array of bank accounts with balance over $100
 // Array example: bankAccounts in /data/data.js
@@ -6,10 +5,14 @@
 
 export function getClientsWithBalanceOverOneHundred(array) {
   // Your code goes here...
-
+  let result = [];
+  for (let elem of array) {
+    if (elem.balance > 100) {
+      result.push(elem);
+    }
+  }
+  return result;
 }
-
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-9"
